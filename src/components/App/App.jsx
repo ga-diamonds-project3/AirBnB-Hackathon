8eee66ba23c955ad5a80068af4079d361a085fe1
Donald Highe <dhighe25@gmail.com>
2016-12-07 11:08:42 -0500

    added nav options

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Nav from '../Nav/Nav.jsx';
 import './App.css';
 
 export default class App extends Component {
   render() {
     return(
       <div>
-        <h1>Heyyy we're alive!!</h1>
+        <div>
+          <h1>Heyyy we're alive!!</h1>
+        </div>
+        <Nav />
       </div>
     )
   }
