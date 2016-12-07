88f7b7d469eac34eaab0af86b54e6c8935fd62a2
Sang Min Na <xsangmin@gmail.com>
2016-12-07 10:36:17 -0500

    get react running with webpack

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const logger  = require('morgan');
+const path    = require('path');
 const PORT    = process.env.PORT || 3000;
-
 
 const app = express();
 
 app.set(logger('dev'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, () => console.log('Server running and listening to', PORT));
