e34b465a5673aaf292881f1b4dfa1b09eb993fbd
Donald Highe <dhighe25@gmail.com>
2016-12-07 11:23:47 -0500

    pulling from upstream

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav.jsx';
 import './App.css';
-import Nav from '../Nav/Nav.jsx';
 const d3 = require('d3');
 
 export default class App extends Component {
   render() {
     return(
-      <div>
-        <div>
-          <h1>Heyyy we're alive!!</h1>
-        </div>
       <div className="App-container">
         <div className="App-nav">
           <Nav />
