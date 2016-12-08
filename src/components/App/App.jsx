342e8c176a7788ebdd43ee90c2561a5dcbb8051b
Nico Cucciniello <ncucciniello@gmail.com>
2016-12-08 14:49:31 -0500

    responsive nav bar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -57,8 +57,6 @@ export default class App extends Component {
   render() {
     return(
       <div className="App-container">
-        <div className="Header-img">
-        </div>
         <div className="App-nav">
           <Nav
             handleSubmit={event => this.handleSubmit(event)}
